b3e48493190b138da6976834edbb8a05125e01cb
J4lR <jairorrosquieta@outlook.com>
2026-02-15 23:15:56 -0800

    I added a App Bar Test with limited functionality

--- test/lib/main.dart
+++ test/lib/main.dart
@@ -12,10 +12,64 @@ class MainApp extends StatelessWidget{
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text("New App"),
+          child: Text("The App"),
         ),
       ),
     );
     
   }
+}
+class AppBarApp extends StatelessWidget{
+  const AppBarApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: AppBarTest());
+  }
+}
+class AppBarTest extends StatelessWidget{
+  const AppBarTest({super.key});
+
+  @override
+    Widget build(BuildContext context){
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text("Planner"),
+          actions: <Widget>[
+            IconButton(icon: const Icon(Icons.add_alert),
+            tooltip: 'Show Snackbar',
+            onPressed: (){
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text('This is a snackbar'))
+              );
+            },),
+            IconButton(
+              icon: const Icon(Icons.navigate_next),
+              tooltip: 'Go to next page',
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute<void>(
+                    builder: (BuildContext context){
+                      return Scaffold(
+                        appBar: AppBar(title: const Text('Next page')),
+                        body: const Center(
+                          child: Text(
+                            'This is the next page',
+                            style: TextStyle(fontSize: 24),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                  );
+              },
+              ),
+          ],
+        ),
+        body: const Center(
+          child: Text('This is the Home Page', style: TextStyle(fontSize: 24)),
+        ),
+      );
+    }
 }
