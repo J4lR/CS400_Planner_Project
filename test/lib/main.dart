c827ec7b5b80a45c36953992770f47ce9dc5b58b
J4lR <jairorrosquieta@outlook.com>
2026-02-23 13:51:27 -0800

    Fixing running issues can now run main on chrome need to fix running on android

--- test/lib/main.dart
+++ test/lib/main.dart
@@ -2,14 +2,18 @@ import 'package:flutter/material.dart';
 //import 'package:intl/date_symbol_data_local.dart';
 import 'package:test/pages/calendar.dart';
 
-
 void main() {
   runApp(MainApp());
 }
 
-class MainApp extends StatelessWidget{
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
+  @override
+  State<MainApp> createState() => _MainPage();
+}
+
+class _MainPage extends State<MainApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,56 +21,65 @@ class MainApp extends StatelessWidget{
         child: Scaffold(
           appBar: AppBar(
             leading: const IconButton(
-              onPressed: null, 
+              onPressed: null,
               icon: Icon(Icons.menu),
               tooltip: "Navigation Menu",
-              ),
+            ),
             title: Text("Planner"),
             actions: <Widget>[
-              IconButton( 
+              IconButton(
                 icon: const Icon(Icons.add_alert),
                 tooltip: "Reminders",
-                onPressed: (){
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text("No Reminders"))
-                  );
+                onPressed: () {
+                  ScaffoldMessenger.of(
+                    context,
+                  ).showSnackBar(const SnackBar(content: Text("No Reminders")));
                 },
               ),
               IconButton(
-                onPressed: null, 
+                onPressed: null,
                 icon: Icon(Icons.add),
                 tooltip: "Add Task",
-                ),
+              ),
               IconButton(
                 icon: const Icon(Icons.arrow_forward),
                 tooltip: "Calendar",
                 onPressed: () => Navigator.push(
                   context,
                   MaterialPageRoute(builder: (_) => const TableCalenderTest()),
-                  ),
                 ),
+              ),
             ],
             backgroundColor: const Color.fromARGB(255, 55, 102, 231),
           ),
-          body: Column(children: [//Should be in a stateful widget since the task will be constantly updating
-            Container(
-              color: Color.fromARGB(253, 6, 30, 151),
-              width: 300.0,
-              alignment: Alignment.center,
-              child: Title(color: Colors.white , child: Text('Task List'),)
-            ),
-            Container(//using GestureDetectors to create unique buttons with containers
-              color: Colors.blue,
-              width: 300.0,
-              height: 50,
-              alignment: Alignment.topRight,
-              child: Center(
-                child: Text("Task"),
+          body: Column(
+            children: [
+              //Should be in a stateful widget since the task will be constantly updating
+              Container(
+                color: Color.fromARGB(253, 6, 30, 151),
+                width: 300.0,
+                alignment: Alignment.center,
+                child: Title(color: Colors.white, child: Text('Task List')),
               ),
-            )
-          ],)
+              Container(
+                //using GestureDetectors to create unique buttons with containers
+                color: Colors.blue,
+                width: 300.0,
+                height: 50,
+                alignment: Alignment.topRight,
+                child: Center(child: Text("Task")),
+              ),
+              ElevatedButton(
+                onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => const TableCalenderTest()),
+                ),
+                child: const Text('View All Task'),
+              ),
+            ],
+          ),
         ),
       ),
-    ); 
+    );
   }
 }
