cb1342037e88a4fbcd82821f6a8846e854e50e51
JuanABCode <jbenazar30@gmail.com>
2026-03-20 20:23:19 -0700

    Fixed Flutter Problem

--- frontend/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ frontend/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\Jairo\flutter\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Jairo\OneDrive\Documents\GitHub\CS400_Planner_Project\test"
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\JB\Documents\GitHub\CS400_Planner_Project\frontend"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
